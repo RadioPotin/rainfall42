c551a42def2197a6a8cf9524b23e93ed724d0ee0
Arthur S <arsciand@student.42.fr>
2022-08-01 11:50:01 +0200

    improve/level3 (#17)
    * Improved README.md
    
    * Reworked pseudocode.c
    
    * Fix spaces

---
+++ level3/pseudocode.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int m;
+
+void    v(void)
+{
+    char buffer[520];
+
+    fgets(buffer, 0x200, stdin);
+    printf(buffer);
+
+    if (m == 0x40)
+    {
+        fwrite("Wait what?!\n", 1, 0xc, stdout);
+        system("/bin/sh");
+    }
+}
+
+int     main(void)
+{
+    v();
+    return (0);
+}
